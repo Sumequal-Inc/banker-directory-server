3a3a3287cf1f98cd1a68bfabbb13a50e3fb04a1b
prashantsayp <prashantkr0111@gmail.com>
2025-05-15 18:39:13 +0530

    lenders-service-backend

--- src/lender/schemas/lender.schema.ts
+++ src/lender/schemas/lender.schema.ts
@@ -1,25 +1,49 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+
 export type LenderDocument = Lender & Document;
+
 @Schema({ timestamps: true })
 export class Lender {
   @Prop({ required: true })
   lenderName: string;
+
   @Prop({ required: true })
   state: string;
+
   @Prop({ required: true })
   city: string;
+
   @Prop({ required: true })
   managerName: string;
-   @Prop({ required: true })
+
+  @Prop({ required: true })
   bankerName: string;
-  @Prop({required:true})
-  email:string;
+
+  @Prop({ required: true })
+  email: string;
+
+  
+  @Prop()
+  rmName: string;
+
+  @Prop()
+  rmContact: string;
+
+  @Prop()
+  asmName: string;
+
+  @Prop()
+  asmContact: string;
+
+  @Prop()
+  rsmName: string;
+
+  @Prop()
+  rsmContact: string;
+
+  @Prop()
+  zsmName: string;
 }
+
 export const LenderSchema = SchemaFactory.createForClass(Lender);
-
-
-
-
-
-
