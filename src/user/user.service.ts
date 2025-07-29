eaadb99c5c0d16683a4f9762bcfd664bb40bb59f
prashantsayp <prashantkr0111@gmail.com>
2025-07-29 13:47:20 +0530

    password-resolve

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,3 @@
-// src/user/user.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -6,25 +5,21 @@ import { User } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './dto/update-user.dto';
-
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-
+  
   async create(dto: CreateUserDto): Promise<User> {
     const hashedPassword = await bcrypt.hash(dto.password, 10); // hash password
     const createdUser = new this.userModel({ ...dto, password: hashedPassword });
     return createdUser.save();
   }
-
   async findByEmail(email: string): Promise<User | null> {
     return this.userModel.findOne({ email });
   }
-
   async findAll(): Promise<User[]> {
     return this.userModel.find();
   }
-
   async update(id: string, data: Partial<UpdateUserDto>): Promise<User> {
     // Only hash the password if it's provided in update
     if (data.password) {
